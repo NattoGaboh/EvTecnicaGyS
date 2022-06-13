013c491a4fa672a5168618f13de401f42408e0a2
NattoGaboh <renato.gvl10@gmail.com>
2022-06-13 13:05:32 -0500

    Create Project/Add Client Model

---
+++ Domain/Models/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvTecnicaGyS.Domain.Models
+{
+    public class Client
+    {
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        public string CodCliente { get; set; }
+        [Required]
+        [Column(TypeName="varchar(200)")]
+        public string NombreCompleto { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(40)")]
+        public string NombreCorto { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(40)")]
+        public string Abreviatura { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(11)")]
+        public string Ruc { get; set; }
+        [Required]
+        [Column(TypeName = "char(1)")]
+        public string Estado { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string GrupoFacturacion { get; set; }
+        [Column(TypeName = "datetime")]
+        public string InactivoDesde { get; set; }
+        [Column(TypeName = "varchar(20")]
+        public string CodigoSAP { get; set; }
+    }
+}
