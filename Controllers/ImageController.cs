7684b61fa7b465d4700b9aea4d453d88f35003d4
NattoGaboh <renato.gvl10@gmail.com>
2022-06-14 00:15:14 -0500

    Fix ImageController

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -1,8 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace EvTecnicaGyS.Controllers
@@ -11,5 +10,25 @@ namespace EvTecnicaGyS.Controllers
     [ApiController]
     public class ImageController : ControllerBase
     {
+        public readonly IAmazonS3 _amazons3;
+        public ImageController(IAmazonS3 amazons3)
+        {
+            _amazons3 = amazons3;
+        }
+
+        // POST api/Image
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] IFormFile formFile)
+        {
+            var putRequest = new PutObjectRequest()
+            {
+                BucketName = "BucketName",   //Here Name of the Bucket S3
+                Key = formFile.FileName,
+                InputStream = formFile.OpenReadStream(),
+                ContentType = formFile.ContentType,
+            };
+            var result = await _amazons3.PutObjectAsync(putRequest);
+            return Ok(result);
+        }
     }
 }
